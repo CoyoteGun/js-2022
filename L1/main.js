a769f4500832005446418e9d6a38f77cb1150d83
CoyoteGun <viktor2411001@gmail.com>
2022-11-01 14:48:04 +0200

    hw1

---
+++ L1/main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let qwe = 'Hello';
+console.log(qwe)
+
+let okten = 'owu';
+console.log(okten)
+
+let asd = 'com';
+console.log(asd)
+
+let index = 'ua';
+console.log(index)
+
+let num = '1, 10, -999, 123, 3.14, 2.7, 16';
+console.log(num)
+
+let tru = 24 > 23;
+console.log(tru)
+
+let fal = 24 < 23;
+console.log(fal)
+
+let middleName = 'Kramar';
+let firsName = 'Viktor';
+let lastName = 'Myroslavovych';
+let person = `${middleName} ${firsName} ${lastName}`;
+console.log(person);
+
+let a = 100;
+console.log(typeof a)
+
+let b = '100';
+console.log(typeof b)
+
+let c = true;
+console.log(typeof c)
+
