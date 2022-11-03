95fbfb503d0b9c077d73dd49c2642e1ab1431c10
CoyoteGun <viktor2411001@gmail.com>
2022-11-03 21:11:39 +0200

    resolve mistake in hw2

--- L2/main.js
+++ L2/main.js
@@ -10,7 +10,16 @@ arr[6] = typeof 11;
 arr[7] = false;
 arr[8] = `object`;
 arr[9] = [666];
-console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
 
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
