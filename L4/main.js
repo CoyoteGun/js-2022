a128f4640ebb93578713d6877f3aecdaae8a93b1
CoyoteGun <viktor2411001@gmail.com>
2022-11-10 15:53:41 +0200

    hw4

---
+++ L4/main.js
@@ -0,0 +1,111 @@
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+
+let smth = [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+];
+for (let i = 0; i < smth.length; i++) {
+    document.write(`<div>text</div>`)
+}
+
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+let ind = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+];
+for (let i = 0; i < ind.length; i++) {
+    document.write(`<div>id - ${ind[i]}</div>`);
+}
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+
+let x = [
+    'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',
+];
+let z = 0;
+while (z < x.length) {
+    document.write(`<h1>header</h1>`);
+    z++;
+}
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+
+let head = [
+    'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',
+];
+let i = 0;
+while (i < head.length) {
+    document.write(`<h1>loops - ${head[i]}</h1>`);
+    i++;
+}
+
+// - Використовуючи данні з масиву, за допомоги document.write та циклу побудувати структуру по шаблону
+
+let listOfItems = ['html',
+    'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+for (let j = 0; j < listOfItems.length; j++) {
+    const listOfItem = listOfItems[j];
+    document.write(`<ul><li>${listOfItem}</li></ul>`);
+    
+}
+
+// Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону  Зробити адекватну стилізацію
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+
+for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    document.write
+    (`<div class="product-card">
+        <h3 class="product-title">${product[`title`]} - ${product[`price`]}</h3>
+        <img src="${product.image}" alt="" class="product-image" >
+    </div>`);
+}
+
+// є масив
+// за допомоги циклу вивести:
+//     - користувачів зі статусом true
+// - користувачів зі статусом false
+// - користувачів які старші за 30 років
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+for (const user of users) {
+    if (user.status)
+        // if (!user.status)
+        // if (user.age > 30)
+    console.log(user);
+}
